996a7a164d220208d1a8cda3fec0eb5a11b3ba39
dgrisham <dmgrisham@gmail.com>
2020-01-27 12:51:04 -0700

    js -> jsx

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ var path = require('path');
 var HtmlWebpackPlugin =  require('html-webpack-plugin');
 
 module.exports = {
-    entry: './app/index.js',
+    entry: './app/index.jsx',
     output: {
         path: path.resolve(__dirname , 'dist'),
         filename: 'bundle.js'
     },
     module: {
       rules: [
-        {test: /\.(js)$/, use: 'babel-loader'},
+        {test: /\.(jsx)$/, use: 'babel-loader'},
         {test: /\.css$/, use: ['style-loader', 'css-loader']}
       ]
     },
